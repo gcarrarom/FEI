db60f61697bbd70b8cf6c0e64c40691a572fd2f7
Guilherme Carraro Martins <guicarraromar@outlook.com>
2015-10-31 18:46:57 -0200

    Funcionando com CrossOver dos dois bits menos significantes para n gerações

--- AlgoritmoGeneticoHerbert/AlgoritmoGeneticoHerbert/Program.cs
+++ AlgoritmoGeneticoHerbert/AlgoritmoGeneticoHerbert/Program.cs
@@ -9,16 +9,23 @@ namespace AlgoritmoGeneticoHerbert
 {
     class TempoDeSistema
     {
-        public string binario { get; set; }
+        public string binario { get; set;}
+        public void SetBinario(string teste) {
+            this.binario = teste;
+            this.tempo = Convert.ToInt32(teste, 2);
+            this.quadrado = this.tempo ^ 2;
+        }
         public int tempo { get; set; }
         public int quadrado { get; set; }
+        public double probabilidade { get; set; }
         public TempoDeSistema(int tempo)
         {
             this.tempo = tempo;
             this.binario = Convert.ToString(tempo, 2);
-            this.quadrado = tempo ^ 2;
+            this.quadrado = tempo * tempo;
         }
     }
+
     class Program
     {
         
@@ -26,6 +33,9 @@ namespace AlgoritmoGeneticoHerbert
         static void Main(string[] args)
         {
             List<TempoDeSistema> tempos = new List<TempoDeSistema>();
+            
+            List<double> roleta = new List<double>();
+            double somatoria;
             string arquivoDeEntrada;
             if (args.Length != 0)
             {
@@ -42,8 +52,69 @@ namespace AlgoritmoGeneticoHerbert
                 tempos.Add(new TempoDeSistema(Convert.ToInt32(tempo)));
             }
 
+            Console.WriteLine("Quantas gerações?");
+            int geracoes = Convert.ToInt32(Console.ReadLine());
+            for (int k = 0; k < geracoes; k++)
+            {
+                somatoria = tempos.Sum(t => t.quadrado);
+                foreach (TempoDeSistema item in tempos)
+                {
+                    item.probabilidade = item.quadrado / somatoria;
+                }
+                somatoria = 0;
+                foreach (var item in tempos)
+                {
+                    roleta.Add(somatoria);
+                    somatoria += item.probabilidade;
+                }
+                roleta.Add(somatoria);
+                List<TempoDeSistema> temp = new List<TempoDeSistema>();
+                temp = tempos;
+                Random rand = new Random();
+                for (int j = 0; j < tempos.Count; j++)
+                {
+                    int i;
+                    double numRandomico = rand.NextDouble();
+                    for (i = 0; numRandomico > roleta[i]; i++) ;
+                    tempos[j] = new TempoDeSistema(temp[i - 1].tempo);
+                }
+                int quantidadeCrossover;
+                if (tempos.Count % 2 == 0)
+                {
+                    quantidadeCrossover = tempos.Count / 2;
+                }
+                else
+                {
+                    quantidadeCrossover = (tempos.Count - 1) / 2;
+                }
+                for (int i = 0; i < quantidadeCrossover; i += 2)
+                {
+                    string temporaria = tempos[i].binario.Substring(tempos[i].binario.Length - 2);
+                    tempos[i].binario = tempos[i].binario.Remove(tempos[i].binario.Length - 2);
+                    tempos[i].SetBinario(tempos[i].binario + tempos[i + 1].binario.Substring(tempos[i + 1].binario.Length - 2));
+                    tempos[i + 1].binario = tempos[i + 1].binario.Remove(tempos[i + 1].binario.Length - 2);
+                    tempos[i + 1].SetBinario(tempos[i + 1].binario + temporaria);
+                }
+                Console.WriteLine("Geração " + (k+1).ToString() + ": " + ParaString(tempos));
+            }
 
+            
+            Console.Read();
 
+        }
+
+        private static string ParaString(List<TempoDeSistema> tempos)
+        {
+            string ultima = "";
+
+            foreach (var item in tempos)
+            {
+                ultima += item.tempo.ToString() + ";";
+            }
+
+            ultima = ultima.Remove(ultima.Length - 1);
+
+            return ultima;
         }
 
         private static string leArquivo(string args)
